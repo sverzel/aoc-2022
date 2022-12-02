7879fa03d60fb518a24da78ebbc126205a07cf27
Stefan Verzel <stefan.a.verzel@capgemini.com>
2022-12-02 21:07:05 +0100

    Using List::Util qw(sum) instead - shorter

--- day2.pl
+++ day2.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
 
 use strict;
+use List::Util qw(sum);
 
 my ($input) = $0 =~ /^([^.]+)/;
 $input .= '_input';
@@ -12,13 +13,5 @@ my @values;
     push @values, join '', /(\w) (\w)/ foreach (split "\n", <$fh>);
 }
 
-sub sum {
-	my $r = shift;
-	my $s = 0;
-	$s += $r->{ $_ } foreach @values;
-	
-	return $s;
-}
-
-print "Score part1: ", sum({AX => 4, AY => 8, AZ => 3, BX => 1, BY => 5, BZ => 9, CX => 7, CY => 2, CZ => 6}), "\n";
-print "Score part2: ", sum({AX => 3, AY => 4, AZ => 8, BX => 1, BY => 5, BZ => 9, CX => 2, CY => 6, CZ => 7}), "\n";
+print "Score part1: ", sum(map {AX => 4, AY => 8, AZ => 3, BX => 1, BY => 5, BZ => 9, CX => 7, CY => 2, CZ => 6}->{$_}, @values), "\n";
+print "Score part2: ", sum(map {AX => 3, AY => 4, AZ => 8, BX => 1, BY => 5, BZ => 9, CX => 2, CY => 6, CZ => 7}->{$_}, @values), "\n";
