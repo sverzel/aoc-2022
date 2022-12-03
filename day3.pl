4599af5f4d82792fbce94c22ca8b0bdaa5d90b3d
Stefan Verzel <stefan.a.verzel@capgemini.com>
2022-12-03 20:32:20 +0100

    Shortened

--- day3.pl
+++ day3.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl -w
 
 use strict;
+use List::Util qw(sum);
+
 my ($input) = $0 =~ /^([^.]+)/;
 $input .= '_input';
 
@@ -11,31 +13,15 @@ my @values;
     push @values, [split //] foreach split "\n", <$fh>;
 }
 
-my %p;
-@p{'a'..'z'} = (1..26);
-@p{'A'..'Z'} = (27..52);
+my %p; @p{'a'..'z', 'A'..'Z'} = (1..52);
 
 sub intersect {
-    my ($a, $b) = @_;
-    my %h;
-    map $h{$_} = 1, @$b;
-    return do { my %s; grep $h{$_} && !$s{$_}++, @$a };
+    my %h; map $h{$_} = 1, @{$_[1]};
+    return do { my %s; grep $h{$_} && !$s{$_}++, @{$_[0]} };
 }
 
-my $s;
-foreach my $rs(@values) {
-    my $l = scalar(@$rs);
-    my @a = (@$rs)[0..$l/2-1];
-    my @b = (@$rs)[$l/2..$l-1];
-    $s += $p{$_} foreach intersect([@a], [@b]);
-}
+print "Sum of priorities: ", sum(map $p{ (intersect([(@$_)[0..@$_/2-1]], [(@$_)[@$_/2..@$_-1]]))[0] }, @values), "\n";
 
-print "Sum of priorities: $s\n";
-
-$s = 0;
-for (my $i = 0; $i < @values; $i += 3) {
-    my ($is) = intersect([intersect($values[$i], $values[$i+1])], $values[$i+2]);
-    $s += $p{ $is };
-}
+my $s = 0; $s += $p{ (intersect([intersect(pop @values, pop @values)], pop @values))[0] } while @values;
 
 print "Badge priorities: $s\n";
