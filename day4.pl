b0e5693eadea6f2e1c0c23e0061493826be8a23b
sverzel <stefan@verzel.se>
2022-12-04 12:38:42 +0100

    Day 4

---
+++ day4.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+
+use strict;
+use List::Util qw(sum);
+
+my ($input) = $0 =~ /^([^.]+)/;
+$input .= '_input';
+
+my @values;
+{
+    local $/;
+    open my $fh, "<$input" or die $!;
+    push @values, [map [split '-'], split ','] foreach split "\n", <$fh>;
+}
+
+print "Number of fully covering pairs: ", sum(map $_->[0][0] <= $_->[1][0] && $_->[0][1] >= $_->[1][1] || $_->[1][0] <= $_->[0][0] && $_->[1][1] >= $_->[0][1] ? 1 : 0, @values), "\n";
+
+print "Number of partly covering pairs: ", sum(map {my %h; map $h{$_} = 1, $_->[0][0]..$_->[0][1]; scalar(grep $h{$_}, $_->[1][0]..$_->[1][1])>0 ? 1 : 0} @values), "\n";
