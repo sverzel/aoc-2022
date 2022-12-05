f6a11b2cbe9334184806d35f48867aa422ec45c4
sverzel <stefan@verzel.se>
2022-12-05 12:10:05 +0100

    Manip both stack lists simultaneously

--- day5.pl
+++ day5.pl
@@ -27,13 +27,8 @@ push @s2, map [@$_], @s1;
 foreach my $instr(@values) {
     my ($n, $f, $t) = $instr =~ /^move (\d+) from (\d+) to (\d+)$/;
     push @{$s1[$t-1]}, pop @{$s1[$f-1]} for 1 .. $n;
-}
-
-print 'Top of stack single move: ', join '', map($_->[-1], @s1), "\n";
-
-foreach my $instr(@values) {
-    my ($n, $f, $t) = $instr =~ /^move (\d+) from (\d+) to (\d+)$/;
     push @{$s2[$t-1]}, splice @{$s2[$f-1]}, scalar(@{$s2[$f-1]})-$n, $n;
 }
 
+print 'Top of stack single move: ', join '', map($_->[-1], @s1), "\n";
 print 'Top of stack multiple move: ', join '', map($_->[-1], @s2), "\n";
