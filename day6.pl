aa7dfa48abb9f8a7ca658a23eed8c32a6f9f6b0c
sverzel <stefan@verzel.se>
2022-12-06 10:16:42 +0100

    Cleaned up

--- day6.pl
+++ day6.pl
@@ -8,16 +8,10 @@ $input .= '_input';
 
 my $s = read_file($input);
 
-my $b = '';
 my $i = 0;
 foreach my $t(4, 14) {
-    do {
-	$b = substr($s, $i++, $t);
-	my %h;
-	@h{split //, $b} = 1;
-	if (scalar(keys %h) == $t) {
-	    print "found type $t: '$b' after ", $i+$t-1, " byte\n";
-	    $b = '';
-	}
-    } while (length($b) == $t);
+    while ($i < length($s)-$t) {
+	my %h; @h{split //, substr($s, $i++, $t)} = 1;
+	do { print "found type $t after ", $i+$t-1, " bytes\n"; last; } if scalar(keys %h) == $t;
+    }
 }
