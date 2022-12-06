59afd60d5941f3ca09d6f81714b64e4afbb9bbb2
sverzel <stefan@verzel.se>
2022-12-06 13:03:46 +0100

    Shortened

--- day6.pl
+++ day6.pl
@@ -8,8 +8,6 @@ my ($input) = $0 =~ /^([^.]+)/;
 my ($s, $i) = (read_file($input . '_input'), 0);
 
 foreach my $t(4, 14) {
-    while ($i < length($s)-$t) {
-	my %h; @h{split //, substr($s, $i++, $t)} = 1;
-	do { print "found type $t after ", $i+$t-1, " bytes\n"; last; } if scalar(keys %h) == $t;
-    }
+    1 while substr($s, $i++, $t) =~ /(.).*\1/;
+    print "found type $t after ", $i+$t-1, " bytes\n"
 }
